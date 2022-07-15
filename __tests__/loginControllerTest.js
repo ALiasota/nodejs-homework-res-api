4a732fa9189dd79ac0c8aa9d1185bcb2888e840f
Andrii Liasota <lyasota.andriy@gmail.com>
2022-07-15 15:13:32 +0300

    test

---
+++ __tests__/loginControllerTest.js
@@ -0,0 +1,14 @@
+const { loginController } = require("../src/controllers/authController");
+
+test("Login test", async () => {
+  const mReq = { body: { email: "avatar@email.com", password: "avatar" } };
+  const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+  await loginController(mReq, mRes);
+
+  expect(mRes.status).toBeCalledWith(200);
+  expect(mRes.token).toEqual(expect.anything());
+  expect(mRes.user.email).toEqual(expect.not.stringContaining(expected));
+  expect(mRes.send.user.password).toEqual(
+    expect.not.stringContaining(expected)
+  );
+});
